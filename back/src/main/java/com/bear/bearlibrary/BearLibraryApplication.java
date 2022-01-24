04fb9802b14f22caba9bbc0e17c2004ce043f49d
墨羽翎玖 <1597867839@qq.com>
2022-01-24 19:28:43 +0800

    初始化后端项目

---
+++ back/src/main/java/com/bear/bearlibrary/BearLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.bear.bearlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearLibraryApplication.class, args);
+    }
+
+}
